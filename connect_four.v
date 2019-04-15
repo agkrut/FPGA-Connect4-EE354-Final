e7837b56840349ce898bd0ed5203032901a80138
Martin Romo <romoma@usc.edu>
2019-04-14 20:23:26 -0700

    Updated top to allow basic display on FPGA and started on VGA module

--- connect_four.v
+++ connect_four.v
@@ -1,81 +1,220 @@
 `timescale 1ns / 1ps
 module connect_four (
-		Left, Right, Select, Reset,
-		Clk,                    
-		selected_col
-	);
-	input Left, Right, Select, Reset, Clk;
-	output [2:0] selected_col;
+        Clk, Reset,
+        Left, Right, Select,
+        board, colors, selected_col, player, game_over, winner
+    );
+    input Left, Right, Select, Reset, Clk;
+    output [41:0] board, colors;
+    output [2:0] selected_col;
+	output player, game_over;
+	output [1:0] winner;
+    
+    reg [41:0] board; //Row i, col j is located at board[7*i+j]
+    /*
+    35 36 37 38 39 40 41
+    28 29 30 31 32 33 34
+    21 22 23 24 25 26 27
+    14 15 16 17 18 19 20
+    07 08 09 10 11 12 13
+    00 01 02 03 04 05 06
+    */
+    reg [41:0] colors; //if board[i]=1 then color is 0 = red, 1 = black
+    reg [2:0] selected_col; //Track selected column
+    reg player; //Player 0 is red, Player 1 is black
+	reg game_over; //Flag to determine if game has ended
+    reg [1:0] winner; //00 = draw, 01 = red, 10 = black
 	
-	reg [2:0] selected_col;
-	reg [1:0] board [5:0] [6:0]; //Make a 6x7 board, 00 = no piece, 01 = red, 10 = black
-	reg player; //Player 0 is red, Player 1 is black
-	reg [2:0] col_depth [6:0]; //Track depth of each column
-	reg [4:0] state;
-	integer col;
-	integer row;
+    reg [2:0] col_depth [6:0]; //Track depth of each column
+    reg [4:0] state;
+    
+    integer index;
+    integer i, j;
 
-	localparam
-	INITIAL 	= 5'b00001,
-	MOVE		= 5'b00010,
-	CHECK_MOVE	= 5'b00100,
-	CHECK_C4	= 5'b01000,
-	WIN			= 5'b10000;
+    localparam
+    INITIAL     = 5'b00001,
+    MOVE        = 5'b00010,
+    CHECK_MOVE  = 5'b00100,
+    CHECK_C4    = 5'b01000,
+    END         = 5'b10000;
 
-	always @(posedge Clk, posedge Reset) begin
-		if (Reset) begin
-        	state <= INITIAL;
-		end
-		else begin : STATE_MACHINE
-			(* full_case, parallel_case *)
-         	case (state)
-         		INITIAL : 
-         		begin
-         			player <= 0;
-         			selected_col <= 3;
-         			//RESET BOARD
-         			for (col=0; col<7; col=col+1) begin
-         				for (row=0; row<6; row=row+1) begin
-         					board[row][col] <= 2'b00;
-         				end
-         				col_depth[col] <= 0;
-         			end
-         			state <= MOVE;
-         			//PRINT NEW GAME
-         		end
-         		MOVE :
-         		begin
-	         		if(Select)
-	         			state <= CHECK_MOVE;
-	         		else
-	         		begin
-						if(Left & !Right & (selected_col > 0))
-	         				selected_col <= selected_col - 1;
-						if(Right & !Left & (selected_col < 6))
-							selected_col <= selected_col + 1;
-	         		end
-         		end
-         		CHECK_MOVE :
-         		begin
-         			if(col_depth[selected_col] != 6)
-         				begin
-         				state <= CHECK_C4;
-         				col_depth[selected_col] <= col_depth[selected_col] + 1;
-         				if(!player)
-         					board[col_depth[selected_col]][selected_col] <= 2'b01;
-         				else
-         					board[col_depth[selected_col]][selected_col] <= 2'b10;
-         				end
-         			else
-         				state <= MOVE;
-         		end
-         		CHECK_C4 :
-         		begin
-         			state <= MOVE;
-         			player <= player ^ 1;
-         			selected_col <= 3;
-				end
-         	endcase
-		end
-	end
+    always @(posedge Clk, posedge Reset) begin
+        if (Reset) begin
+            state <= INITIAL;
+            board <= 42'bX;
+            colors <= 42'bX;
+            selected_col <= 3'bX;
+            player <= 1'bX;
+            for(index=0; index<7; index=index+1) begin
+                col_depth[index] <= 3'bX;
+            end
+            winner <= 2'bX;
+        end
+        else begin : STATE_MACHINE
+            (* full_case, parallel_case *)
+            case (state)
+                INITIAL : 
+                begin
+							game_over = 0;
+                    player <= 0;
+                    selected_col <= 3; //Start in middle
+                    winner <= 0;
+                    //RESET BOARD
+                    for (index=0; index<42; index=index+1) begin
+                        board[index] <= 0;
+                        colors[index] <= 0;
+                    end
+                    //RESET DEPTHS
+                    for(index=0; index<7; index=index+1) begin
+                        col_depth[index] <= 0;
+                    end
+                    state <= MOVE;
+                    //PRINT NEW GAME
+                end
+                MOVE :
+                begin
+                    if(Select)
+                        state <= CHECK_MOVE;
+                    else
+                    begin
+                        if(Left & !Right & (selected_col > 0))
+                            selected_col <= selected_col - 1'b1;
+                        if(Right & !Left & (selected_col < 6))
+                            selected_col <= selected_col + 1'b1;
+                    end
+                end
+                CHECK_MOVE :
+                begin
+                    if(col_depth[selected_col] != 6)
+                    begin
+                        state <= CHECK_C4;
+                        col_depth[selected_col] <= col_depth[selected_col] + 1'b1;
+                        board[7*col_depth[selected_col]+selected_col] <= 1;
+                        if(player) //Check if chip is colored black
+                            colors[7*col_depth[selected_col]+selected_col] <= 1;
+                    end
+                    else
+                        state <= MOVE;
+                end
+                CHECK_C4 :
+                begin
+                    //Check columns
+                    /*
+                    O   O   X
+                    O   X   X
+                    X   X   X
+                    X   X   X
+                    X   X   O
+                    X,  O,  O
+                    */
+                    for(i=0; i<7; i=i+1) begin //Column
+                        for(j=0; j<3; j=j+1) begin //Row
+                            //Check that there's discs in each spot
+                            if(board[7*(j)+i] & board[7*(j+1)+i] & board[7*(j+2)+i] & board[7*(j+3)+i]) begin
+                                //Check all 4 are the same color
+                                if(!((colors[7*(j)+i]^colors[7*(j+1)+i]) |
+                                    (colors[7*(j+1)+i]^colors[7*(j+2)+i]) |
+                                    (colors[7*(j+2)+i]^colors[7*(j+3)+i]))) begin
+                                    game_over = 1;
+                                end
+                            end
+                        end
+                    end
+                    
+                    //Check rows
+                    /*
+                    X X X X O O O,
+                    O X X X X O O,
+                    O O X X X X O,
+                    O O O X X X X,
+                    */
+                    for(i=0; i<6; i=i+1) begin //Row
+                        for(j=0; j<4; j=j+1) begin //Column
+                            //Check that there's discs in each spot
+                            if(board[7*i+(j)] & board[7*i+(j+1)] & board[7*i+(j+2)] & board[7*i+(j+3)]) begin
+                                //Check all 4 are the same color
+                                if(!((colors[7*i+(j)]^colors[7*i+(j+1)]) |
+                                    (colors[7*i+(j+1)]^colors[7*i+(j+2)]) |
+                                    (colors[7*i+(j+2)]^colors[7*i+(j+3)]))) begin
+                                    game_over = 1;
+                                end
+                            end
+                        end
+                    end
+
+                    //Check Diagonals BottomLeft to TopRight
+                    /*
+                    O O O X O O O
+                    O O X O O O O
+                    O X O X O O X
+                    X O X O O X O
+                    O X O O X O O
+                    X O O X O O O
+                    */
+                    for(i=0; i<4; i=i+1) begin //Starting column
+                        for(j=0; j<3; j=j+1) begin //Starting row
+                            if(board[7*(j)+(i)] & board[7*(j+1)+(i+1)] &
+                                board[7*(j+2)+(i+2)] & board[7*(j+3)+(i+3)]) begin
+                                //Check all 4 are the same color
+                                if(!((colors[7*(j)+(i)]^colors[7*(j+1)+(i+1)]) |
+                                    (colors[7*(j+1)+(i+1)]^colors[7*(j+2)+(i+2)]) |
+                                    (colors[7*(j+2)+(i+2)]^colors[7*(j+3)+(i+3)]))) begin
+                                    game_over = 1;
+                                end
+                            end
+                        end
+                    end
+
+                    //Check Diagonals BottomRight to TopLeft
+                    /*
+                    X O O X O O O
+                    O X O O X O O
+                    X O X O O X O
+                    O X O X O O X
+                    O O X O O O O
+                    O O O X O O O
+                    */
+                    for(i=0; i<4; i=i+1) begin //Starting column
+                        for(j=3; j<6; j=j+1) begin //Starting row
+                            if(board[7*(j)+(i)] & board[7*(j-1)+(i+1)] &
+                                board[7*(j-2)+(i+2)] & board[7*(j-3)+(i+3)]) begin
+                                //Check all 4 are the same color
+                                if(!((colors[7*(j)+(i)]^colors[7*(j-1)+(i+1)]) |
+                                    (colors[7*(j-1)+(i+1)]^colors[7*(j-2)+(i+2)]) |
+                                    (colors[7*(j-2)+(i+2)]^colors[7*(j-3)+(i+3)]))) begin
+                                    game_over = 1;
+                                end
+                            end
+                        end
+                    end
+                    
+                    if(game_over) begin
+                        state <= END;
+                        winner <= { player, ~player };
+                    end
+                    else begin
+                        //Determine if all rows are filled up and is a draw
+                        game_over=1;
+                        for(i=0; i<7; i=i+1) begin
+                            if(col_depth[i] != 6)
+                                game_over=0;
+                        end
+                        if(game_over) begin
+                            state <= END;
+                            winner <= 2'b00;
+                        end
+                        else begin
+                            state <= MOVE;
+                            player <= player ^ 1;
+                            selected_col <= 3;
+                        end
+                    end
+                end
+                END :
+                begin
+                
+                end
+            endcase
+        end
+    end
 endmodule
--- connect_four_top.v
+++ connect_four_top.v
@@ -1,57 +1,187 @@
 `timescale 1ns / 1ps
 module connect_four_top (
-		MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS, // Disable the three memory chips
-		ClkPort,              // the 100 MHz incoming clock signal
-		Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0, //LEDs to show selected location
-		An0, An1, An2, An3, //Disable SSD anodes
-		BtnU, BtnL, BtnR, BtnC      // the Left, Right, and Center buttons
-	);
+        MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS, // Disable the three memory chips
+        ClkPort,             				//100 MHz clock signal
+        Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0, 	//LEDs
+        An3, An2, An1, An0, 				//SSD Anodes
+        Cg, Cf, Ce, Cd, Cc, Cb, Ca, Dp, 	//SSD Cathodes
+        Sw5, Sw4, Sw3, Sw2, Sw1, Sw0, 		//Switches
+        BtnU, BtnL, BtnR, BtnC,      		//Left, Right, and Center buttons
+		VGA_Red0, VGA_Green0, VGA_Blue0,		//VGA RGB pins
+		VGA_Hsync, VGA_Vsync				//VGA sync pins
+    );
 
-	/* INPUT */
-	// Clock & Reset I/O
-	input		ClkPort;
-	input BtnL, BtnR, BtnC, BtnU;
+    /* INPUT */
+    // Clock & Reset I/O
+    input ClkPort;
+    input BtnL, BtnR, BtnC, BtnU;
+    input Sw5, Sw4, Sw3, Sw2, Sw1, Sw0;
 
-	/* OUTPUT */
-	// Control signals on Memory chips 	(to disable them)
-	output 	MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS;
-	assign {MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS} = 5'b11111;
+    /* OUTPUT */
+    // Control signals on Memory chips  (to disable them)
+    output  MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS;
+    assign {MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS} = 5'b11111;
 
-	output 	Ld0, Ld1, Ld2, Ld3, Ld4, Ld5, Ld6;
-	output 	An0, An1, An2, An3;
-	assign {An0, An1, An2, An3} = 4'b1111;
+    output Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0;
+    output An3, An2, An1, An0;
+    output Cg, Cf, Ce, Cd, Cc, Cb, Ca, Dp;
+	output VGA_Red0, VGA_Green0, VGA_Blue0;
+	output VGA_Hsync, VGA_Vsync;
 
-	wire		Reset, ClkPort;
-	wire		board_clk, sys_clk;
+    wire Reset, ClkPort;
+    wire board_clk, game_clk, vga_clk;
+    wire [1:0] ssdscan_clk;
+    reg [26:0]  DIV_CLK;
 
-	wire Left, Right, Select;
-	wire [2:0] selected_col;
+    wire Left, Right, Select;
+    wire [41:0] board;
+    wire [41:0] colors;
+    wire [2:0] selected_col;
+	wire player, game_over;
+	wire [1:0] winner;
 
-	reg [26:0]	DIV_CLK;
-	BUFGP BUFGP1 (board_clk, ClkPort); 	
+	//SSD
+    wire [5:0]  SWITCHES;
+    reg [3:0]   SSD;
+    reg [3:0]  SSD3, SSD2, SSD1, SSD0;
+    reg [6:0]   SSD_CATHODES;
+	
+	//VGA
+	wire VGA_Red0, VGA_Green0, VGA_Blue0;
+	wire VGA_Hsync, VGA_Vsync;
 
-	/*
-		Divide the clock
-	*/
-	always @(posedge board_clk, posedge Reset) 	
-    begin							
+    /*
+        Divide the clock
+    */
+    BUFGP BUFGP1 (board_clk, ClkPort);  
+    always @(posedge board_clk, posedge Reset)  
+    begin                           
         if (Reset)
-		DIV_CLK <= 0;
+            DIV_CLK <= 0;
         else
-		DIV_CLK <= DIV_CLK + 1'b1;
+            DIV_CLK <= DIV_CLK + 1'b1;
     end
 
-    assign	sys_clk = DIV_CLK[23];
-
+    /*
+    Create connect four module
+    */
+    assign game_clk = DIV_CLK[25];
     assign Left = BtnL;
     assign Right = BtnR;
     assign Select = BtnC;
-	assign Reset = BtnU;
+    assign Reset = BtnU;
     connect_four connect_four_mod(
-		.Left(Left), .Right(Right), .Select(Select), .Reset(Reset),
-		.Clk(sys_clk), .selected_col(selected_col)
-	);
+        .Clk(game_clk), .Reset(Reset),
+        .Left(Left), .Right(Right), .Select(Select),
+        .board(board), .colors(colors), .selected_col(selected_col),
+		.player(player), .game_over(game_over), .winner(winner)
+    );
 
+	/*
+	Create VGA module
+	*/
+	assign vga_clk = DIV_CLK[1]; //25MHz clock
+	vga_display vga_display_mod(
+		.Clk(vga_clk), .Reset(Reset),
+		.VGA_Hsync(VGA_Hsync), .VGA_Vsync(VGA_Vsync),
+		.VGA_Red0(VGA_Red0), .VGA_Green0(VGA_Green0), .VGA_Blue0(VGA_Blue0),
+		.board(board), .colors(colors), .selected_col(selected_col),
+		.player(player), .game_over(game_over), .winner(winner)
+	);
+	
+    /*
+    Control SSD Display with switches
+    */
+    assign SWITCHES = { Sw5, Sw4, Sw3, Sw2, Sw1, Sw0 };
+    always @ (SWITCHES, board)
+    begin : DISPLAY_ROW
+        case(SWITCHES)
+            6'b000001: 
+				begin
+				SSD0 = { board[5], colors[5], board[6], colors[6] };
+				SSD1 = { board[3], colors[3], board[4], colors[4] };
+				SSD2 = { board[1], colors[1], board[2], colors[2] };
+				SSD3 = { 2'b00 , board[0], colors[0] };
+				end
+            6'b000010: 
+				begin
+				SSD0 = { board[12], colors[12], board[13], colors[13] };
+				SSD1 = { board[10], colors[10], board[11], colors[11] };
+				SSD2 = { board[8], colors[8], board[9], colors[9] };
+				SSD3 = { 2'b00 , board[7], colors[7] };
+				end
+            6'b000100: 
+				begin
+				SSD0 = { board[19], colors[19], board[20], colors[20] };
+				SSD1 = { board[17], colors[17], board[18], colors[18] };
+				SSD2 = { board[15], colors[15], board[16], colors[16] };
+				SSD3 = { 2'b00 , board[14], colors[14] };
+				end
+            6'b001000: 
+				begin
+				SSD0 = { board[26], colors[26], board[27], colors[27] };
+				SSD1 = { board[24], colors[24], board[25], colors[25] };
+				SSD2 = { board[22], colors[22], board[23], colors[23] };
+				SSD3 = { 2'b00 , board[21], colors[21] };
+				end
+            6'b010000: 
+				begin
+				SSD0 = { board[33], colors[33], board[34], colors[34] };
+				SSD1 = { board[31], colors[31], board[32], colors[32] };
+				SSD2 = { board[29], colors[29], board[30], colors[30] };
+				SSD3 = { 2'b00 , board[28], colors[28] };
+				end
+            6'b100000: 
+				begin
+				SSD0 = { board[40], colors[40], board[41], colors[41] };
+				SSD1 = { board[38], colors[38], board[39], colors[39] };
+				SSD2 = { board[36], colors[36], board[37], colors[37] };
+				SSD3 = { 2'b00 , board[35], colors[35] };
+				end
+            default: 
+				begin
+				SSD0 = 4'b0000;
+				SSD1 = 4'b0000;
+				SSD2 = 4'b0000;
+				SSD3 = 4'b0000;
+				end
+		endcase
+    end
+
+    assign ssdscan_clk = DIV_CLK[19:18];
+    assign An0  = !(ssdscan_clk == 2'b00);
+    assign An1  = !(ssdscan_clk == 2'b01);
+    assign An2  = !(ssdscan_clk == 2'b10);
+    assign An3  = !(ssdscan_clk == 2'b11);
+
+    always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+    begin : SSD_SCAN_OUT
+        case (ssdscan_clk) 
+            2'b00: SSD = SSD0;
+            2'b01: SSD = SSD1;
+            2'b10: SSD = SSD2;
+            2'b11: SSD = SSD3;
+			default: SSD = 4'bXXXX;
+        endcase 
+    end
+
+    always @ (SSD)
+    begin : SSD_DISPLAY
+        case({SSD[3], SSD[1]})
+            2'b00: SSD_CATHODES = 7'b1111111;
+            2'b01: SSD_CATHODES = {1'b1, ~SSD[0], SSD[0], 4'b1111};
+            2'b10: SSD_CATHODES = {4'b1111, SSD[2], ~SSD[2], 1'b1};
+            2'b11: SSD_CATHODES = {1'b1, ~SSD[0], SSD[0], 1'b1, SSD[2], ~SSD[2], 1'b1};
+        endcase
+    end
+
+
+    assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES, 1'b1};
+
+    /*
+    Control LEDs
+    */
+    //Light up LED to show selected column
     assign Ld6 = (selected_col == 0);
     assign Ld5 = (selected_col == 1);
     assign Ld4 = (selected_col == 2);
